336f94321d495adfdd63dba378c35a1ac91fecb5
Siaxo <Thomas.Hellsten.91@gmail.com>
2022-01-03 15:59:42 +0100

    Added Customer amount and Account amount

--- AdminBankSystem/Pages/Index.cshtml.cs
+++ AdminBankSystem/Pages/Index.cshtml.cs
@@ -1,20 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdminBankSystem.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
 
 namespace AdminBankSystem.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly BankContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, BankContext context)
         {
             _logger = logger;
+            _context = context;
         }
+
+        public int CustomerAmount { get; set; }
+        public int AccountAmount { get; set; }
+        public int BalanceAmount { get; set; }
+
+
+        
+
+        
 
         public void OnGet()
         {
-
+            CustomerAmount = _context.Customers.Count();
+            AccountAmount = _context.Accounts.Count();
         }
     }
 }
--- AdminBankSystem/Program.cs
+++ AdminBankSystem/Program.cs
@@ -1,8 +1,13 @@
+using AdminBankSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddDbContext<BankContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
