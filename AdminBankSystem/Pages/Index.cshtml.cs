069fac9a786628e40c9185421c9edac0008c9b08
Thomas H <85164423+Siaxo@users.noreply.github.com>
2022-01-04 14:43:18 +0100

    Added Customer image

--- AdminBankSystem/Pages/Index.cshtml.cs
+++ AdminBankSystem/Pages/Index.cshtml.cs
@@ -19,17 +19,39 @@ namespace AdminBankSystem.Pages
         public int CustomerAmount { get; set; }
         public int AccountAmount { get; set; }
         public int BalanceAmount { get; set; }
-
-
         
 
-        
+        public class CustomerViewModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string SocialSecurtity { get; set; }
+            public string City { get; set; }
+        }
+
+        public List<CustomerViewModel> Customers { get; set; }
+
+
+
+
 
         public void OnGet()
         {
             CustomerAmount = _context.Customers.Count();
             AccountAmount = _context.Accounts.Count();
             BalanceAmount = _context.Transactions.Count();
+
+            Customers = _context.Customers
+                .Take(50)
+                .Select(r => new CustomerViewModel
+            {
+                Id = r.CustomerId,
+                Name = r.Givenname,
+                Address = r.Streetaddress,
+                SocialSecurtity = r.NationalId,
+                City = r.City
+            }).ToList();
         }
     }
 }
